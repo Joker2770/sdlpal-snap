37b5ba649db03728147458cdcc8f2f62d98a7d77
Joker2770 <1214220480@qq.com>
2022-06-22 15:50:18 +0800

    init commit.

---
+++ launcher.sh
@@ -0,0 +1,26 @@
+#
+#    This is a launch script.
+#    Copyright (C) 2022 Jintao Yang <yjt950840@outlook.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#    launcher.sh:
+#       launch script.
+#       launch GUI program by calling commands on Linux.
+#
+
+#!/bin/bash
+
+cd $HOME
+$SNAP/usr/local/bin/sdlpal
